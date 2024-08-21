707d147b85f21ec241f5003412595903c37bc496
Sangamesh Basavaraj Mugadur <sangameshbm71998@gmail.com>
2024-08-22 02:03:32 +0530

    restricting domain user for Google SSO

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,44 +1,30 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
-
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
-
   def google_oauth2
-    user = User.from_omniauth(auth)
+    # Extract the user's email and domain
+    email = auth.info.email
+    domain = email.split('@').last
 
-    if user.present?
-      sign_out_all_scopes
-      flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
-      sign_in_and_redirect user, event: :authentication
+    # Define the allowed domain
+    allowed_domain = 'elitmus.com'
+
+    # Check if the user is from the allowed domain
+    if domain == allowed_domain
+      user = User.from_omniauth(auth)
+
+      if user.present?
+        sign_out_all_scopes
+        flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
+        sign_in_and_redirect user, event: :authentication
+      else
+        flash[:alert] = t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{email} is not authorized."
+        redirect_to new_user_session_path
+      end
     else
-      flash[:alert] =
-        t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
+      # If the user's domain is not allowed, show an error message and redirect
+      flash[:alert] = t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{email} is not authorized."
       redirect_to new_user_session_path
     end
   end
-
-  # More info at:
-  # https://github.com/heartcombo/devise#omniauth
-
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
-
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
-
-  # protected
-
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
 
   protected
 
@@ -51,15 +37,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   private
-
-  # def from_google_params
-  #   @from_google_params ||= {
-  #     uid: auth.uid,
-  #     email: auth.info.email,
-  #     full_name: auth.info.name,
-  #     avatar_url: auth.info.image
-  #   }
-  # end
 
   def auth
     @auth ||= request.env['omniauth.auth']
